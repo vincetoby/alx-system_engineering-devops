735b52759de584a6509e13a12c38b5864734f4d5
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-04-09 23:58:22 +0100

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of
+subscribers for a given subreddit
+"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    this func queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    usr_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=usr_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
